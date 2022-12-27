bfc2731b3a15a02bb69ccc03bdc43a23ae227957
Tom <tomlefrere@hotmail.fr>
2022-12-27 22:22:33 +0100

    MAJ de mon avancement - OUI JE L'AI !

--- src/main/java/com/alexscode/teaching/TestHSortInterest4DistanceUltimate.java
+++ src/main/java/com/alexscode/teaching/TestHSortInterest4DistanceUltimate.java
@@ -5,9 +5,7 @@ import com.alexscode.teaching.tap.Objectives;
 import com.alexscode.teaching.tap.TAPSolver;
 import com.alexscode.teaching.utilities.Element;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class TestHSortInterest4DistanceUltimate implements TAPSolver {
     @Override
@@ -29,20 +27,49 @@ public class TestHSortInterest4DistanceUltimate implements TAPSolver {
         Collections.reverse(demo);
 
         // on ajoute ensuite les queries qui maximisent l'interet et qui respectent les contraintes de temps et de distance
+        result.add(demo.get(0).getIndex());
+        demo.remove(0);
         while (obj.distance(result) <= ist.getMaxDistance() && obj.time(result) <= ist.getTimeBudget()) {
-            for (int i = 0; i < demo.size()/2; i++) {
-                if (ist.getDistances()[demo.get(i).getIndex()][demo.get(i++).getIndex()] > ist.getDistances()[demo.get(i++).getIndex()][demo.get(i+2).getIndex()]) {
-                    checkIsExist(demo, result, i++, i+2);
-                }
-                else {
-                    checkIsExist(demo, result, i, i++);
+            int index_min = 0;
+            // On fait une map pour stocker toute les distance liées a i et on prend la plus petite
+            Map<Integer, Double> map = new HashMap<>();
+            for (int j = 0; j < demo.size(); j++) {
+                if (ist.getDistances()[result.get(result.size()-1)][demo.get(j).getIndex()] != 0) {
+                    map.put(demo.get(j).getIndex(), ist.getDistances()[result.get(result.size()-1)][demo.get(j).getIndex()]);
                 }
             }
-            break;
 
-
+            // On prend l'index de la valeur minimale
+            index_min = Collections.min(map.entrySet(), Map.Entry.comparingByValue()).getKey();
+            // On regarde si il n'y a pas d'autre valeur avec max 50% de la valeur minimale
+            List<Integer> list = new ArrayList<>();
+            for (Map.Entry<Integer, Double> entry : map.entrySet()) {
+                if (entry.getValue() <= Collections.min(map.values())) {
+                    list.add(entry.getKey());
+                }
+            }
+            // On prend l'index de la query qui maximise l'interet
+            if (list.size() > 1) {
+                for (int j = 0; j < list.size(); j++) {
+                    if (ist.getInterest()[list.get(j)] >= ist.getInterest()[index_min]) {
+                        index_min = list.get(j);
+                    }
+                }
+            } else index_min = list.get(0);
+            // On ajoute la query a la liste de resultat
+            if (!result.contains(index_min)) {
+                result.add(index_min);
+                // entier qui est l'index de demo qui correspond a la query qui a été ajouté a result
+                int index = 999;
+                for (int j = 0; j < demo.size(); j++) {
+                    if (demo.get(j).getIndex() == index_min) {
+                        index = j;
+                    }
+                }
+                demo.remove(index);
+                map.clear();
+            }
         }
-        result.remove(result.size() - 1);
         result.remove(result.size() - 1);
         return result;
     }
@@ -61,15 +88,5 @@ public class TestHSortInterest4DistanceUltimate implements TAPSolver {
         return null;
     }
 
-//    private static class void ultimateRecursive(List<Element> demo, List<Integer> result, Instance ist, int i) {
-//        for (int i = 0; i < demo.size()/2; i++) {
-//            if (ist.getDistances()[demo.get(i).getIndex()][demo.get(i++).getIndex()] > ist.getDistances()[demo.get(i++).getIndex()][demo.get(i+2).getIndex()]) {
-//                checkIsExist(demo, result, i++, i+2);
-//            }
-//            else {
-//                checkIsExist(demo, result, i, i++);
-//            }
-//        }
-//    }
 }
 
